54f90198c88908583b9fdb9a93e179aa0628b92f
Zhabskyi <poltavagaz@gmail.com>
2020-02-06 19:02:59 -0700

    Changed styling of top section

--- client/src/components/topSection/TopSection.js
+++ client/src/components/topSection/TopSection.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import classes from './TopSection.module.scss';
-import photo_1 from '../../static/Camera.png';
+import white from '../../static/white.png';
 import photo_2 from '../../static/Camera_straight.png';
 
 const TopSection = () => {
   return (
     <div className={classes.container}>
-      <div className={classes.block_info}>
         <div>
           <h3>Just a buck for photo!</h3>
         </div>
@@ -14,8 +13,6 @@ const TopSection = () => {
           It is almost free give away. You will get high quality photos of HUGE
           size
         </p>
-      </div>
-      <div className={classes.block_img}></div>
     </div>
   );
 };
