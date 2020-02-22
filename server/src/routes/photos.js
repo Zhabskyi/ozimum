75eaadfd5f55c440bea1528c98f96a66edfd4875
Zhabskyi <poltavagaz@gmail.com>
2020-02-21 18:32:03 -0700

    Fix issue with uploading big files with NGINX

--- server/src/routes/photos.js
+++ server/src/routes/photos.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const database = require('../db/database.js');
-const privateKey = require('../keys');
+const Jimp = require('jimp');
 
-require("dotenv").config();
-const multer = require("multer");
-const AWS = require("aws-sdk");
+require('dotenv').config();
+const multer = require('multer');
+const AWS = require('aws-sdk');
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
@@ -22,55 +22,72 @@ module.exports = db => {
     }
   });
 
-    //Add photo
-    router.post("/photos", upload.single("file"), async (req, res) => {
-      let info = req.body;
-  
-      try {
-        const file = req.file;
-        const s3FileURL = process.env.AWS_Uploaded_File_URL_LINK;
-  
-        let s3bucket = new AWS.S3({
-          accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-          region: process.env.AWS_REGION
+  //Add photo
+  router.post('/photos', upload.single('file'), async (req, res) => {
+    let info = req.body;
+    console.log("Info",info)
+    console.log("Image",req.file)
+
+    try {
+      const image = req.file;
+
+
+      let file = await Jimp.read(image.buffer)
+        .then(image => {
+          console.log("IMAGE",image)
+          return image.resize(250, 250);
+          // Do stuff with the image.
+        })
+        .catch(err => {
+          console.log(err);
+          // Handle an exception.
         });
-  
-        //Where you want to store your file
-  
-        const params = {
-          Bucket: process.env.AWS_BUCKET_NAME,
-          Key: file.originalname,
-          Body: file.buffer,
-          ContentType: file.mimetype,
-          ACL: "public-read"
-        };
-  
-        s3bucket.upload(params, async (err, data) => {
-          try {
-            if (err) {
-              res.status(500).json({ error: true, Message: err });
-            } else {
-              const newFileUploaded = {
-                description: req.body.description,
-                fileLink: s3FileURL + file.originalname,
-                s3_key: params.Key
-              };
-              info = { ...info, photo: newFileUploaded.fileLink };
-              // Add all info to database after store picture to S3
-              const photos = await database.addPhoto(db, info);
-              res.send(photos);
-            }
-          } catch (err) {
-            console.error(err.message);
-            res.status(500).send("Server Error");
+
+        console.log("FILE", file)
+
+      const s3FileURL = process.env.AWS_Uploaded_File_URL_LINK;
+
+      let s3bucket = new AWS.S3({
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: process.env.AWS_REGION
+      });
+
+      //Where you want to store your file
+
+      const params = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: file.originalname,
+        Body: file.buffer,
+        ContentType: file.mimetype,
+        ACL: 'public-read'
+      };
+
+      s3bucket.upload(params, async (err, data) => {
+        try {
+          if (err) {
+            res.status(500).json({ error: true, Message: err });
+          } else {
+            const newFileUploaded = {
+              description: req.body.description,
+              fileLink: s3FileURL + file.originalname,
+              s3_key: params.Key
+            };
+            info = { ...info, photo: newFileUploaded.fileLink };
+            // Add all info to database after store picture to S3
+            const photos = await database.addPhoto(db, info);
+            res.send(photos);
           }
-        });
-      } catch (err) {
-        console.error(err.message);
-        res.status(500).send("Server Error");
-      }
-    });
+        } catch (err) {
+          console.error(err.message);
+          res.status(500).send('Server Error');
+        }
+      });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  });
 
   return router;
 };
