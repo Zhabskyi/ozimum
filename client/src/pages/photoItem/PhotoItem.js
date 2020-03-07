e666b15e13bb7fe0d0585e6b88b5d27be48bc9b4
Zhabskyi <poltavagaz@gmail.com>
2020-03-07 10:55:23 -0700

    Fix layout issues in PhotoItem page

--- client/src/pages/photoItem/PhotoItem.js
+++ client/src/pages/photoItem/PhotoItem.js
@@ -20,44 +20,43 @@ const PhotoItem = props => {
     <>
       {!!photo ? (
         <div className={classes.container}>
-          <div className='container__left'>
-            <div className='container__left__prev'>
-              <div className='container__left__prev__descript'>
-                <h1 className='container__left__prev__descript_title'>
+          <div className={classes.container__left}>
+            <div className={classes.container__left__prev}>
+              <div className={classes.container__left__prev__descript}>
+                <h1 className={classes.container__left__prev__descript_title}>
                   {photo[0].title}
                 </h1>
-                <div className='container__left__prev__descript_text'>
+                <div className={classes.container__left__prev__descript_text}>
                   {photo[0].description}
                 </div>
               </div>
               <div
-                style={{ backgroundImage: photo[0].photo }}
-                className='container__left__prev__blur'
+                style={{ backgroundImage: `url(${photo[0].photo})` }}
+                className={classes.container__left__prev__blur}
               ></div>
-              <div className='container__left__prev__card'>
-                <div className='container__left__prev__card__wrapper'>
+              <div className={classes.container__left__prev__card}>
+                <div className={classes.container__left__prev__card__wrapper}>
                   <img
                     src={photo[0].photo}
                     alt={photo[0].title}
-                    className='container__left__prev__card__wrapper_img'
+                    className={classes.container__left__prev__card__wrapper_img}
                   />
                 </div>
               </div>
             </div>
           </div>
-          <div className='container__divider'></div>
-          <div className='container__right'>
-            <div className='container__right__panel'>
-              <div className='container__right__panel__purchase'>
-                <h2 className='container__right__panel__purchase_header'>
-                  PURCHASE THE PHOTO
-                </h2>
-                <div className='container__right__panel__purchase__body'>
-                  <section className='container__right__panel__purchase__body__top'></section>
-                </div>
+          <div className={classes.container__right}>
+            <div className={classes.container__right__purchase}>
+              <p className={classes.container__right__purchase_header}>
+                PURCHASE THE PHOTO
+              </p>
+              <div className={classes.container__right__purchase__body}>
+                <section
+                  className={classes.container__right__purchase__body__top}
+                ></section>
               </div>
-              <div className='container__right__panel__details'></div>
             </div>
+            <div className={classes.container__right__details}></div>
           </div>
         </div>
       ) : null}
