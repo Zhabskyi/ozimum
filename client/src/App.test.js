6fac8617065c8224d742e90aec5ad5c88de69911
Zhabskyi <poltavagaz@gmail.com>
2020-03-09 15:15:54 -0600

    Add store provider to test file

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './index';
 import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders header text', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
   const headerText = getByText('OZIMUM');
   expect(headerText).toBeInTheDocument();
 });
